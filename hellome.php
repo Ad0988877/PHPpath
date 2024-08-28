56f36b774e20198e4aea0f07c0ea42c7dd0250b0
Ad0988877 <164032165+Ad0988877@users.noreply.github.com>
2024-08-27 21:13:47 -0500

    Add files via upload

---
+++ hellome.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = "Ayden Downing ";
+$Class = [
+    'degree' => "CIS",
+    'transport' => "bus",
+    'github' => "https://github.com/Ad0988877/variables.git"
+];
+echo ($name);
+print_r($Class);
